c7fe0a777d1bb660a4094c910a5cc0224d5a91ba
AfeefAhmer <s2401102@edu.bc.fi>
2026-02-03 12:12:42 +0200

    Tehtävä 3.4 - Nuoli-luokan viimeistely

--- Tehtävä 3 Nuoli/Program.cs
+++ Tehtävä 3 Nuoli/Program.cs
@@ -5,18 +5,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Valitse kärki (0 = puu, 1 = teräs, 2 = timantti)");
-            Kärki kärki = (Kärki)int.Parse(Console.ReadLine());
+            Console.WriteLine("Tervetuloa nuolikauppaan.");
+            Console.WriteLine("Haluatko:");
+            Console.WriteLine("1. Teettää nuolen tilaustyönä?");
+            Console.WriteLine("2. Ostaa valmiin nuolen?");
+            Console.Write("Valinta: ");
+            Nuoli? Ostos=null;
+            int valinta = int.Parse(Console.ReadLine());
+            if (valinta == 2)
+            {
+                Console.WriteLine("Valitse valmis Nuoli");
+                Console.WriteLine("1. Eliittinuoli");
+                Console.WriteLine("2. Aloittelijanuoli");
+                Console.WriteLine("3. Perusnuoli");
+                Console.Write("Valinta: ");
+                int ValmisValinta = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Valitse perä (0 = lehti, 1 = kanansulka, 2 = kotkansulka)");
-            Perä perä = (Perä)int.Parse(Console.ReadLine());
+                switch (ValmisValinta) 
+                {
+                    case 1:Ostos = Nuoli.LuoEliittiNuoli(); break;
+                    case 2:Ostos = Nuoli.LuoAloittelijaNuoli();break;
+                    case 3:Ostos = Nuoli.LuoPerusNuoli() ;break;
+                }
+                Console.WriteLine($"Nuolen hinta on {Ostos.PalautaHinta():0.00} kultarahaa");
+            }
 
-            Console.WriteLine("Valitse VarrenPituus (60 - 100 cm");
-            int pituus = int.Parse(Console.ReadLine());
 
-            Nuoli nuoli = new Nuoli(kärki, perä, pituus);
+            else
+            {
+                Console.WriteLine("Valitse kärki (0 = puu, 1 = teräs, 2 = timantti)");
+                Kärki kärki = (Kärki)int.Parse(Console.ReadLine());
 
-            Console.WriteLine($"Nuolen hinta on {nuoli.PalautaHinta():0.00} kultarahaa");
+                Console.WriteLine("Valitse perä (0 = lehti, 1 = kanansulka, 2 = kotkansulka)");
+                Perä perä = (Perä)int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Valitse VarrenPituus (60 - 100 cm");
+                int pituus = int.Parse(Console.ReadLine());
+
+                Nuoli nuoli = new Nuoli(kärki, perä, pituus);
+
+                Console.WriteLine($"Nuolen hinta on {nuoli.PalautaHinta():0.00} kultarahaa");
+            }
         }
         class Nuoli
         {
@@ -37,6 +66,20 @@
                 Kärki = kärki;
                 Perä = perä;
                 VarrenPituus = varrenPituus;
+            }
+
+            //Staattiset Metodit
+            public static Nuoli LuoEliittiNuoli()
+            {
+                return new Nuoli(Kärki.timantti,Perä.kotkansulka,100);
+            }
+            public static Nuoli LuoAloittelijaNuoli()
+            {
+                return new Nuoli(Kärki.puu, Perä.kanansulka, 70);
+            }
+            public static Nuoli LuoPerusNuoli()
+            {
+                return new Nuoli(Kärki.teräs, Perä.kanansulka, 85);
             }
             public double PalautaHinta()
             {
