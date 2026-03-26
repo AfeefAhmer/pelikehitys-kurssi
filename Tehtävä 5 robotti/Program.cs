cd1be0e5956b224a5c8f946b0759f482f24576c7
AfeefAhmer <s2401102@edu.bc.fi>
2026-03-26 11:55:34 +0200

    tehtävä 5.2 robotti

--- Tehtävä 5 robotti/Program.cs
+++ Tehtävä 5 robotti/Program.cs
@@ -16,7 +16,7 @@ namespace Tehtävä_5_robotti
                 robotti.Käskyt[i] = LuoKäsky(syöte);
             }
             robotti.Suorita();
-            static RobottiKäsky? LuoKäsky(string? syöte)
+            static IRobottiKäsky? LuoKäsky(string? syöte)
             {
                 return syöte?.ToLower() switch
                 {
@@ -31,7 +31,7 @@ namespace Tehtävä_5_robotti
             }
         }
     }
-    public abstract class RobottiKäsky
+    public interface IRobottiKäsky
     {
         public abstract void Suorita(Robotti robotti);
     }
@@ -40,33 +40,33 @@ namespace Tehtävä_5_robotti
         public int X { get; set; }
         public int Y { get; set; }
         public bool OnKäynnissä { get; set; }
-        public RobottiKäsky?[] Käskyt { get; } = new RobottiKäsky?[3];
+        public IRobottiKäsky?[] Käskyt { get; } = new IRobottiKäsky?[3];
 
         public void Suorita()
         {
-            foreach (RobottiKäsky? käsky in Käskyt)
+            foreach (IRobottiKäsky? käsky in Käskyt)
             {
                 käsky?.Suorita(this);
                 Console.WriteLine($"[{X} {Y} {OnKäynnissä}]");
             }
         }
-        public class Käynnistä : RobottiKäsky
+        public class Käynnistä : IRobottiKäsky
         {
-            public override void Suorita(Robotti robotti)
+            public void Suorita(Robotti robotti)
             {
                 robotti.OnKäynnissä = true;
             }
         }
-        public class Sammuta: RobottiKäsky
+        public class Sammuta: IRobottiKäsky
         {
-            public override void Suorita(Robotti robotti)
+            public  void Suorita(Robotti robotti)
             {
                 robotti.OnKäynnissä = false;
             }
         }
-        public class YlösKäsky: RobottiKäsky
+        public class YlösKäsky: IRobottiKäsky
         {
-            public override void Suorita(Robotti robotti)
+            public  void Suorita(Robotti robotti)
             {
                 if (robotti.OnKäynnissä)
                 {
@@ -74,9 +74,9 @@ namespace Tehtävä_5_robotti
                 }
             }
         }
-        public class AlasKäsky : RobottiKäsky
+        public class AlasKäsky : IRobottiKäsky
         {
-            public override void Suorita(Robotti robotti)
+            public void Suorita(Robotti robotti)
             {
                 if (robotti.OnKäynnissä)
                 {
@@ -84,9 +84,9 @@ namespace Tehtävä_5_robotti
                 }
             }
         }
-        public class VasenKäsky : RobottiKäsky
+        public class VasenKäsky : IRobottiKäsky
         {
-            public override void Suorita(Robotti robotti)
+            public  void Suorita(Robotti robotti)
             {
                 if (robotti.OnKäynnissä)
                 {
@@ -94,9 +94,9 @@ namespace Tehtävä_5_robotti
                 }
             }
         }
-        public class OikeaKäsky : RobottiKäsky
+        public class OikeaKäsky : IRobottiKäsky
         {
-            public override void Suorita(Robotti robotti)
+            public void Suorita(Robotti robotti)
             {
                 if (robotti.OnKäynnissä)
                 {
